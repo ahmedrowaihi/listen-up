4f0ff23a0a4aba89125aabc94092fc22c86ae108
Ahmed Rowaihi <ahmedrowaihi1@gmail.com>
2023-08-14 13:44:09 +0300

    👌 IMPROVE(minimize): migrate to cloudflared-tunnel package :d

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,58 @@
 #!/usr/bin/env node
-import yargsParser from 'yargs-parser';
 import chalk from 'chalk';
-import { startTunnel } from './cloudflared-tunnel';
-import { runServer } from './log-server';
+import { startTunnel } from 'cloudflared-tunnel';
+import type { Request, Response } from 'express';
 
-const yargsParserOptions = {
-	alias: { p: 'port', d: 'domain' },
-	describe: {
-		p: 'Port to listen on',
-		d: 'Domain to route to the SSH tunnel (e.g. example.com)',
-	},
-	default: {
-		p: 80,
-		d: '',
-	},
-};
+const args = process.argv.slice(2);
 
-const options = yargsParser(process.argv.slice(2), yargsParserOptions) as unknown as { p: number; d: string };
+function getArgValue(char: string, name: string) {
+	const charIdx = args.findIndex((arg) => arg === char);
+	const nameIdx = args.findIndex((arg) => arg === name);
+	if (charIdx !== -1 && args[charIdx + 1]) return args[charIdx + 1];
+	else if (nameIdx !== -1 && args[nameIdx + 1]) return args[nameIdx + 1];
+	return '';
+}
 
-options.p = !isNaN(options.p) ? options.p : 80;
+function hasArg(char: string, name: string) {
+	return args.includes(char) || args.includes(name);
+}
 
-runServer(options, () => {
-	console.log(chalk.blue(`Server listening on port localhost:${options.p}`));
-	if (options.d) startTunnel(options);
+const port = parseInt(getArgValue('-p', '--port'), 10) || 80;
+const startTunnelFlag = hasArg('-c', '--cloudflare');
+
+function handleRequest(req: Request, res: Response) {
+	try {
+		console.log(chalk.blue(`Query params: ${JSON.stringify(req.query)}`));
+		res.send(`Received ${req.method} request for ${req.originalUrl}`);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+}
+
+import express from 'express';
+import morgan from 'morgan-body';
+
+function runServer(callback: () => void) {
+	console.log('Starting server...');
+	const app = express();
+	morgan(app, {
+		logReqUserAgent: false,
+		logRequestBody: true,
+		logResponseBody: false,
+		maxBodyLength: 10000,
+	});
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+	app.all('*', handleRequest);
+	return app.listen(port, callback); // Return the server instance
+}
+
+runServer(() => {
+	console.log(chalk.blue(`Server listening on port localhost:${port}`));
+	if (startTunnelFlag) {
+		startTunnel({
+			host: 'https://localhost',
+			port: port,
+		});
+	}
 });
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export type Domain = string;
-export type Tunnel = {
-	id: string;
-	name: string;
-};
